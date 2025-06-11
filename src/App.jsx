6f865bd83ffd922eed6161b250625daed78bb23e
Ali Hosseini <a.hosseini9@outlook.com>
2025-06-11 20:59:36 +0330

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router";
 import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import NotFoundPage from "./pages/NotFoundPage";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
-    <div>
-      <h1>پروژه دیوار</h1>
-      <AuthPage />
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
       <Toaster position="top-center" reverseOrder={false} />
-    </div>
+    </>
   );
 }
 
---
+++ src/router/router.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function router() {
+  return (
+    <Routes></Routes>
+  )
+}
+
+export default router
